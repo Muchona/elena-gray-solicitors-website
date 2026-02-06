fe3a8e0eaf38e29d54763b4ae7b8b18b9a2d79f2
Muchona <mucho055@gmail.com>
2026-02-06 21:51:47 +0000

    Fix: Applied universal relative paths for asset resolution on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/elena-gray-solicitors-website/',
+  base: './',
 })
